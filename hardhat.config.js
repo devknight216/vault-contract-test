dc9db75b929f38c7288913dff7026825af48bd2f
devknight216 <andionisie7@gmail.com>
2024-05-05 21:47:19 +0100

    feat: hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,17 @@
+require("@nomicfoundation/hardhat-ethers");
+
 /** @type import('hardhat/config').HardhatUserConfig */
+const { vars } = require("hardhat/config");
+
+const  INFURA_API_KEY = vars.get("INFURA_API_KEY");
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    }
+  }
 };
